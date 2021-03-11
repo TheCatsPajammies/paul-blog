84c5190987107143e371ead9ae163b584ac00864
Paul Thompson <paul.t.thompson83@gmail.com>
2021-03-11 16:54:51 -0600

    Edit: Made small changes to default text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `TheCatsPajammies`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Paul Thompson`,
+      summary: `is a Med Tech and Front End Developer out of Saint Louis, MO`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
